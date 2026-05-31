6b41df3c4b8958c50d0e219bb523638d0975a352
Pragalbh <pragalbhsharma11@gmail.com>
2026-05-31 15:35:04 +0530

    feat: tailwind setup and clean App.tsx

---
+++ cheatcode-frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-4xl font-bold text-white">Cheatcode 🔥</h1>
+    </div>
+  )
+}
+
+export default App
